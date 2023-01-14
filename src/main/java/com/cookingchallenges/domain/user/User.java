8ce83d5e0650ea0bd5cf5db37e25a4bc65f2f29f
Piotr Szumański <piotr.szumanski@orange.com>
2023-01-15 00:09:38 +0100

    User authorization

--- src/main/java/com/cookingchallenges/domain/user/User.java
+++ src/main/java/com/cookingchallenges/domain/user/User.java
@@ -1,8 +1,11 @@
 package com.cookingchallenges.domain.user;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Objects;
 
 @Entity
@@ -13,7 +16,7 @@ import java.util.Objects;
 @EqualsAndHashCode
 @ToString
 //@Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,8 +27,8 @@ public class User {
     @Column(nullable = false, length=50, unique = true)
     private String email;
 
-//    @Column(nullable = false, length=256, unique = true)
-//    private String password;
+    @Column(nullable = false)
+    private String password;
 
     @Column
     @Enumerated(EnumType.STRING)
@@ -45,4 +48,33 @@ public class User {
         this.about = about;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/cookingchallenges/domain/user/UserFacade.java
+++ src/main/java/com/cookingchallenges/domain/user/UserFacade.java
@@ -1,8 +1,6 @@
 package com.cookingchallenges.domain.user;
 
-import com.cookingchallenges.domain.comment.Comment;
 import com.cookingchallenges.domain.comment.CommentDAO;
-import com.cookingchallenges.domain.content.Content;
 import com.cookingchallenges.domain.content.ContentDAO;
 import com.cookingchallenges.domain.user.dto.EditUser;
 import com.cookingchallenges.domain.user.dto.PostUser;
@@ -10,18 +8,22 @@ import com.cookingchallenges.domain.user.dto.UserDTO;
 import com.cookingchallenges.domain.user.exception.UserNotFoundException;
 import com.cookingchallenges.mappers.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserFacade {
+public class UserFacade implements UserDetailsService {
 
     private final UserDAO userDao;
     private final CommentDAO commentDAO;
     private final ContentDAO contentDAO;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public UserDTO getUserById(Long id) {
         User user = userDao.findById(id).orElseThrow(() ->
@@ -37,7 +39,9 @@ public class UserFacade {
 
     @Transactional
     public Long postUser(PostUser postUser) {
-        return userDao.save(new User(postUser.name(), postUser.email(), postUser.about()));
+        User user = new User(postUser.name(), postUser.email(), postUser.about());
+        user.setPassword(passwordEncoder.encode(postUser.password()));
+        return userDao.save(user);
     }
 
     @Transactional
@@ -66,5 +70,16 @@ public class UserFacade {
         user.setRank(rank);
         userDao.save(user);
         return getUserById(id);
+    }
+
+    public User getUserByEmail(String email) {
+        return userDao.findByEmail(email).orElseThrow(() ->
+                new UserNotFoundException("User (email=" + email +") does not exist"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userDao.findByEmail(email).orElseThrow(() ->
+                new UserNotFoundException("User (email=" + email +") does not exist"));
     }
 }
