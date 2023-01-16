78baae3deed2b4bf5228af650a447c8e8a500b92
Piotr Szumański <piotr.szumanski@orange.com>
2023-01-16 09:32:17 +0100

    Endpoint for presentation

--- src/main/java/com/cookingchallenges/mappers/UserMapper.java
+++ src/main/java/com/cookingchallenges/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package com.cookingchallenges.mappers;
 
 import com.cookingchallenges.domain.user.User;
 import com.cookingchallenges.domain.user.dto.UserDTO;
+import com.cookingchallenges.domain.user.dto.UserWithPassDTO;
 
 public class UserMapper {
 
@@ -13,6 +14,17 @@ public class UserMapper {
                 user.getId(),
                 user.getName(),
                 user.getEmail(),
+                user.getGrade().toString(),
+                user.getAbout()
+        );
+    }
+
+    public static UserWithPassDTO mapPass(User user) {
+        return new UserWithPassDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
                 user.getGrade().toString(),
                 user.getAbout()
         );
