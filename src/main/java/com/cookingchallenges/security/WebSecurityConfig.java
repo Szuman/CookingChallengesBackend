75f14133112425191ec2582f6487c55193ec484a
Szuman <piszuman@gmail.com>
2023-04-19 11:09:55 +0200

    User Class Diagrams

--- src/main/java/com/cookingchallenges/security/WebSecurityConfig.java
+++ src/main/java/com/cookingchallenges/security/WebSecurityConfig.java
@@ -53,7 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        AuthenticationFilter authenticationJwtTokenFilter = new AuthenticationFilter(userFacade);
         http
                 .cors()
                 .and()
