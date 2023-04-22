2a5bb45c697f340369e03ed3a6507636799532b9
Piotr Szumański <piotr.szumanski@orange.com>
2023-04-22 18:58:04 +0200

    Correcting Exception Handling

--- src/main/java/com/cookingchallenges/domain/content/dto/EditContent.java
+++ src/main/java/com/cookingchallenges/domain/content/dto/EditContent.java
@@ -5,5 +5,7 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 public record EditContent(
-        @Size(min = 3, max = 48, message = "Title must have between 3 and 48 characters") String title, List<String> products, String description) {
+        @Size(min = 3, max = 48, message = "Title must have between 3 and 48 characters") String title,
+        List<String> products,
+        @Size(max = 255, message = "Description must have not more than 255 characters") String description) {
 }
